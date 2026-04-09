22ad5ea427f8c3a4ff6d6f92e3038b7d9c408a5b
qukatz22 <vektor@viper.hr>
2026-04-09 10:40:33 +0200

    pero1

---
+++ 0_DevEx/ToolbarForm1.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vektor._0_DevEx
+{
+   public partial class ToolbarForm1:DevExpress.XtraEditors.XtraForm
+   {
+      public ToolbarForm1()
+      {
+         InitializeComponent();
+      }
+   }
+}
